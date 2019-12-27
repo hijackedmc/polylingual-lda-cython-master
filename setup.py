446cd0e31312c4a2aa02fa55ceab8fc5c10c7759
chao ma <ustcaimc@gmail.com>
2019-12-27 14:29:22 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,7 @@ from Cython.Distutils import build_ext
 ext_type = Extension("polylda",
                  sources=["polylda.pyx",
                           "gamma.c"])
-#
-# ext_type = Extension("polylda_type",
-#                      sources=["polylda.pyx",
-#                               "gamma.c"])
-# 如果这样会爆出错误： LINK : error LNK2001: 无法解析的外部符号 initpolylda_type
 
 setup(name="polylda",
       cmdclass = {'build_ext': build_ext},
       ext_modules = cythonize([ext_type]))
-# setup(ext_modules = cythonize([ext_type]))
